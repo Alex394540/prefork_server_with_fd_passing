0b6446b88b819103692f92ee0009429c33eb7aaf
Alex394540 <alibaba394540@gmail.com>
2020-11-26 11:22:27 +0300

    Update main.c
    Commented code deleted

--- main.c
+++ main.c
@@ -76,9 +76,6 @@ int main(int argc, char ** argv)
 {
     char * ip, dir;
     int port, opt;
-    // char * dir;
-    // int opt;
-    
     while ((opt = getopt(argc, argv, ":h:p:d:")) != -1)
     {
         switch (opt)
